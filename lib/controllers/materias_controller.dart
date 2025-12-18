a3689cdaaf6d267b8e0fb643772c5b00db890cd0
cesarposso <cesarpossoe2006@gmail.com>
2025-12-18 11:17:53 -0500

    controllers

---
+++ lib/controllers/materias_controller.dart
@@ -0,0 +1,21 @@
+import '../models/materia.dart';
+
+class MateriasController {
+  final List<Materia> materias = [];
+
+  void agregarMateria({required String nombre, int? creditos}) {
+    final clean = nombre.trim();
+    if (clean.isEmpty) {
+      throw ArgumentError('El nombre no puede estar vacío');
+    }
+    materias.add(Materia(nombre: clean, creditos: creditos));
+  }
+
+  void eliminarMateria(Materia materia) {
+    materias.remove(materia);
+  }
+
+  void agregarNota(Materia materia, double nota) {
+    materia.agregarNota(nota);
+  }
+}
